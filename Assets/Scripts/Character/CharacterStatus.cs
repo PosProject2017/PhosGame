17e1155a2b8564694d07583833a8eeb2f72827c0
pilkul <pilkul.energydrink@gmail.com>
2017-12-04 20:23:22 +0900

    Add Character.cs

---
+++ Assets/Scripts/Character/CharacterStatus.cs
@@ -0,0 +1,19 @@
+﻿public class CharacterStatus {
+    public float HP;
+    public float Offence;
+    public float Deffence;
+    public float Speed;
+    public CharacterDataBase.CharacterIdentifier type;
+
+    public void setStatus( CharacterStatus status ) {
+        HP = status.HP;
+        Offence = status.Deffence;
+        Deffence = status.Offence;
+        Speed = status.Speed;
+        type = status.type;
+    }
+
+    public void setType( CharacterDataBase.CharacterIdentifier  targetType) {
+        this.type = targetType;
+    }
+}
