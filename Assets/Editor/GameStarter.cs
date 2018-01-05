0271afc01177b05f9f43cebccc42ebf0c79b4c77
pilkul <pilkul.energydrink@gmail.com>
2018-01-05 15:20:26 +0900

    デバッグ時のスタートシーンを作った ctrl(command) + 0

---
+++ Assets/Editor/GameStarter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GameStarter : MonoBehaviour {
+
+    [MenuItem("Tools/PlayGame %0")]
+    public static void PlayFromPrelaunchScene()
+    {
+    
+        // プレイ中ならば停止する
+        if (EditorApplication.isPlaying == true) {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+        
+        // 再生したいシーンの読み込み->再生
+        EditorApplication.SaveCurrentSceneIfUserWantsTo ();
+        EditorApplication.OpenScene ("Assets/Scenes/DebugBoot.unity");
+        EditorApplication.isPlaying = true;
+    }
+}
